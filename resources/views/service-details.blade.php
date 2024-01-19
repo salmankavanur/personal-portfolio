0802e8c1a3fb2e2ca502b49a94bf02de3c696b9d
Salman Kavanur <mpsalman2011@gmail.com>
2024-01-19 16:05:54 +0530

    Update service-details.blade.php

--- resources/views/service-details.blade.php
+++ resources/views/service-details.blade.php
@@ -9,26 +9,20 @@
 
   <title>Service Details</title>
 
-  <!-- Fav Icon -->
-  <link rel="icon" type="image/x-icon" href="../assets/imgs/logo/favicon.png">
-
-
   <link href="https://fonts.googleapis.com/css2?family=Macondo&display=swap" rel="stylesheet">
 
-
+  <!-- Fav Icon -->
+ <link rel="icon" type="image/x-icon" href="{{ asset('assets/imgs/logo/favicon.png') }}">
 
   <!-- All CSS files -->
-  <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../assets/css/all.min.css">
-  <link rel="stylesheet" href="../assets/css/swiper-bundle.min.css">
-  <link rel="stylesheet" href="../assets/css/progressbar.css">
-  <link rel="stylesheet" href="../assets/css/meanmenu.min.css">
-  <link rel="stylesheet" href="../assets/css/magnific-popup.css">
-  <link rel="stylesheet" href="../assets/css/basictable.min.css">
-  <link rel="stylesheet" href="../assets/css/master.css">
-  <link rel="stylesheet" href="../style.css">
-
-
+  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/progressbar.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/meanmenu.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/master.css') }}">
+  <link rel="stylesheet" href="{{ asset('style.css') }}">
 
 </head>
 
