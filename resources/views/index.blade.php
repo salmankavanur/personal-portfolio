8df4d96c9d5a4c86c9b35a2849269929978053e6
Salman Kavanur <mpsalman2011@gmail.com>
2024-05-31 19:15:08 +0530

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -59,11 +59,20 @@
   svg { width: 75px; margin-top: 1em; }
 </style>
 
-<article>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 202.24 202.24"><defs><style>.cls-1{fill:#fff;}</style></defs><title>Asset 3</title><g id="Layer_2" data-name="Layer 2"><g id="Capa_1" data-name="Capa 1"><path class="cls-1" d="M101.12,0A101.12,101.12,0,1,0,202.24,101.12,101.12,101.12,0,0,0,101.12,0ZM159,148.76H43.28a11.57,11.57,0,0,1-10-17.34L91.09,31.16a11.57,11.57,0,0,1,20.06,0L169,131.43a11.57,11.57,0,0,1-10,17.34Z"/><path class="cls-1" d="M101.12,36.93h0L43.27,137.21H159L101.13,36.94Zm0,88.7a7.71,7.71,0,1,1,7.71-7.71A7.71,7.71,0,0,1,101.12,125.63Zm7.71-50.13a7.56,7.56,0,0,1-.11,1.3l-3.8,22.49a3.86,3.86,0,0,1-7.61,0l-3.8-22.49a8,8,0,0,1-.11-1.3,7.71,7.71,0,1,1,15.43,0Z"/></g></g></svg>
-    <h1>We&rsquo;ll be back soon!</h1>
-    <div>
-        <p>Sorry for the inconvenience. We&rsquo;re performing some maintenance at the moment. If you need to you can always follow us on <a href="https://www.linkedin.com/in/salmanmp/">Linkedin</a> for updates, otherwise we&rsquo;ll be back up shortly!</p>
-        <p>- Salman MP -</p>
-    </div>
-</article>
+    <article>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 202.24 202.24">
+            <!-- Your SVG path here -->
+        </svg>
+        <h1>We'll be back soon!</h1>
+        <div>
+            <p>Sorry for the inconvenience. I'm currently performing some maintenance on my personal website. You can always follow me on <a href="https://www.linkedin.com/in/salmanmp/">LinkedIn</a> for updates, or check back shortly!</p>
+            <p>- Salman MP -</p>
+            <!-- Add your social media icons here -->
+            <ul>
+                <li><a href="https://www.instagram.com/your_instagram_profile"><img src="instagram-icon.png" alt="Instagram"></a></li>
+                <li><a href="https://www.linkedin.com/in/salmanmp/"><img src="linkedin-icon.png" alt="LinkedIn"></a></li>
+                <li><a href="https://github.com/your_github_profile"><img src="github-icon.png" alt="GitHub"></a></li>
+                <li><a href="https://www.behance.net/your_behance_profile"><img src="behance-icon.png" alt="Behance"></a></li>
+            </ul>
+        </div>
+    </article>
