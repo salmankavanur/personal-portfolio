aaeb33ec97a81267c4104c8fdec48bb21693f193
Salman Kavanur <mpsalman2011@gmail.com>
2024-06-01 06:48:54 +0530

    Create maintanance-mode.blade.php

---
+++ resources/views/maintanance-mode.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Experienced and passionate Web and Graphic Designer based in Kerala, India with over 7 years of expertise in both fields. My strong portfolio features a variety of design projects, including brochures, flyers, posters, and social media designs. Proficient in Adobe Suite (Photoshop, Illustrator, Premiere, After Effects, and InDesign) and Canva, I excel at creating visually stunning and effective marketing materials, as well as modern, user-friendly websites.">
+
+  <title>Salman MP - Experienced and Passionate Web and Graphic Designer</title>
+
+  <!-- Open Graph Meta Tags -->
+  <meta property="og:title" content="Salman MP - Experienced and Passionate Web and Graphic Designer">
+  <meta property="og:description" content="Experienced and passionate Web and Graphic Designer based in Kerala, India with over 7 years of expertise in web and graphic design, videography, video editing, photography, and more.">
+  <meta property="og:image" content="{{ asset('assets/imgs/hero/personal-photo-bg-low.webp') }}">
+  <meta property="og:url" content="{{ url()->current() }}">
+  <meta property="og:type" content="website">
+
+  <!-- Twitter Card Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:title" content="Salman MP - Experienced and Passionate Web and Graphic Designer">
+  <meta name="twitter:description" content="Experienced and passionate Web and Graphic Designer based in Kerala, India with over 7 years of expertise in web and graphic design, videography, video editing, photography, and more.">
+  <meta name="twitter:image" content="{{ asset('assets/imgs/hero/personal-photo-bg-low.webp') }}">
+
+  <!-- Structured Data -->
+  <script type="application/ld+json">
+  {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    "name": "Salman MP",
+    "jobTitle": "Web and Graphic Designer",
+    "description": "Experienced and passionate Web and Graphic Designer based in Kerala, India with over 7 years of expertise in web and graphic design, videography, video editing, photography, and more.",
+    "url": "https://salmanmp.me",
+    "sameAs": [
+      "https://www.instagram.com/salman_kavanur",
+      "https://www.linkedin.com/in/salmanmp"
+      "https://github.com/salmankavanur"
+      "https://www.behance.net/salmanmp"
+      "https://www.facebook.com/salmankavanoor",
+      "https://twitter.com/salmankavanur",
+    ],
+    "image": "{{ asset('assets/imgs/hero/personal-photo-bg-low.webp') }}",
+    "address": {
+      "@type": "Malappuram",
+      "addressLocality": "Kerala",
+      "addressRegion": "IN",
+      "addressCountry": "India"
+    }
+  }
+  </script>
+
+  <!-- Fav Icon -->
+  <link rel="icon" type="image/x-icon" href="{{ asset('assets/imgs/logo/site-logo-light.svg') }}">
+  
+  <!-- Font Awesome CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    
+<style>
+  html, body { padding: 0; margin: 0; width: 100%; height: 100%; }
+  * {box-sizing: border-box;}
+  body { text-align: center; padding: 0; background: #d6433b; color: #fff; font-family: Open Sans; }
+  h1 { font-size: 50px; font-weight: 100; text-align: center;}
+  body { font-family: Open Sans; font-weight: 100; font-size: 20px; color: #fff; text-align: center; display: -webkit-box; display: -ms-flexbox; display: flex; -webkit-box-pack: center; -ms-flex-pack: center; justify-content: center; -webkit-box-align: center; -ms-flex-align: center; align-items: center;}
+  article { display: block; width: 700px; padding: 50px; margin: 0 auto; }
+  a { color: #fff; font-weight: bold; text-decoration: none;}
+  a:hover { text-decoration: none; }
+  svg { width: 75px; margin-top: 1em; }
+  .social-icons a { color: #fff; margin: 0 10px; font-size: 24px; }
+</style>
+
+</head>
+<body>
+  <article>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 202.24 202.24">
+          <!-- Your SVG path here -->
+      </svg>
+      <h1>I'll be back soon!</h1>
+      <div>
+          <p>Sorry for the inconvenience. I'm currently performing some maintenance on my personal website. You can always follow me on <a href="https://www.linkedin.com/in/salmanmp/">LinkedIn</a> for updates, or check back shortly!</p>
+          <p>- Salman MP -</p>
+           <!-- Social media icons -->
+           <div class="social-icons">
+              <a href="https://www.instagram.com/salman_kavanur" target="_blank"><i class="fab fa-instagram"></i></a>
+              <a href="https://www.linkedin.com/in/salmanmp" target="_blank"><i class="fab fa-linkedin"></i></a>
+              <a href="https://github.com/salmankavanur" target="_blank"><i class="fab fa-github"></i></a>
+              <a href="https://www.behance.net/salmanmp" target="_blank"><i class="fab fa-behance"></i></a>
+           </div>
+      </div>
+  </article>
+</body>
+</html>
