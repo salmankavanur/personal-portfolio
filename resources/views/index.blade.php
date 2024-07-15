dd960b1f98037d7333ca22611fa05b290d26c323
Salman MP <mpsalman2011@gmail.com>
2024-07-15 18:05:33 +0530

    Google Tag Manager Integrated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,6 +17,14 @@
       })(window, document, "clarity", "script", "n7qijx2n1w");
   </script>
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-W8Q69KG6');</script>
+  <!-- End Google Tag Manager -->
+
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-P8NYSJC65X"></script>
 <script>
@@ -182,6 +190,13 @@
 
 
 <body class="font-heading-estedad">
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-W8Q69KG6');</script>
+  <!-- End Google Tag Manager -->
 
   <!-- Preloader -->
   <div id="preloader">
